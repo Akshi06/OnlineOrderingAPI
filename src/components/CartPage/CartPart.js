d9fcb21b118a50fbd2a6d3e0969e814deafb80c3
Akshi06 <akshi.20191141@iit.ac.lk>
2021-10-28 15:35:23 +0530

    new

--- src/components/CartPage/CartPart.js
+++ src/components/CartPage/CartPart.js
@@ -5,12 +5,13 @@ function CartPart() {
     console.log(A[1].soupItem.productId);
     for(let i = 1; i < A.length ; i++){
        console.log(A[i].soupItem.productName);
+        return (
+            <div>
+                
+            </div>
+        );
     }
-    return (
-        <div>
-            
-        </div>
-    );
+    
 }
 
 export default CartPart;
