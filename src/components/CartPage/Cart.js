047d7bc00a6602b49904550073c9bcc9c051de5e
Akshi06 <akshi.20191141@iit.ac.lk>
2021-10-28 15:39:28 +0530

    new

--- src/components/CartPage/Cart.js
+++ src/components/CartPage/Cart.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './Cart.css';
+import CartPart from './CartPart';
+
 
 
 
@@ -38,51 +40,10 @@ class Cart extends Component {
   render() {
     return (
 		<div>
-					<div className="container">
+				<div className="container">
 					<h1>YUMMY TUMMY CART</h1>
-					<table id="cart" className="table table-hover table-condensed">
-								<thead>
-									<tr>
-										<th className="th1">Product</th>
-										<th className="th2">Price</th>
-										<th className="th3">Quantity</th>
-										<th  className="th4">Subtotal</th>
-										<th className="th5"></th>
-									</tr>
-								</thead>
-								<tbody>
-									<tr>
-										<td data-th="Product">
-											<div className="row">
-												<div className="col-sm-2 hidden-xs"><img src="" alt="..." className="img-responsive"/></div>
-												<div className="col-sm-10">
-													<h4 className="nomargin">pk</h4>
-													<p>dis</p>
-												</div>
-											</div>
-										</td>
-										<td data-th="Price">price</td>
-										<td data-th="Quantity">
-											<p className="form-control text-center">q</p>
-										</td>
-										<td data-th="Subtotal" className="text-center">1.99</td>
-										{/* <td class="actions" data-th="">
-											<button class="btn btn-info btn-sm"><i class="fa fa-refresh"></i></button>
-											<button class="btn btn-danger btn-sm"><i class="fa fa-trash-o"></i></button>								
-										</td> */}
-									</tr>
-								</tbody>
-								<tfoot>
-									{/* <tr cclassNamelass="visible-xs">
-										<td className="text-center"><strong>Total 1.99</strong></td>
-									</tr> */}
-									<tr>
-										<td colSpan="3" className="hidden-xs"></td>
-										<td className="hidden-xs text-center"><strong>Total $1.99</strong></td>
-									</tr>
-								</tfoot>
-						</table>
 				</div>
+				<CartPart />
 				
 		</div>
     );
